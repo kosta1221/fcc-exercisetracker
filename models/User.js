ff26f422a87b55a4ae601a757a018ee685e08e68
kosta1221 <76045173+kosta1221@users.noreply.github.com>
2021-03-12 15:14:13 +0200

    User model for our db

---
+++ models/User.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const uri = process.env.MONGODB_URI;
+
+console.log("connecting to", uri);
+
+mongoose
+	.connect(uri, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+		useFindAndModify: false,
+		useCreateIndex: true,
+	})
+	.then(() => {
+		console.log("connected to MongoDB");
+	})
+	.catch((error) => {
+		console.log("error connecting to MongoDB", error.message);
+	});
+
+const userSchema = new mongoose.Schema({
+	name: {
+		type: String,
+		minLength: 3,
+		unique: true,
+		required: true,
+	},
+});
+
+module.exports = mongoose.model("User", userSchema);
